e9568a41b4389f60ffa0579aed582dc8e67c74ac
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-29 22:58:33 +0200

    initial commit

---
+++ src/PLoM_surrogate/generators.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def generator_U(n_samples):
+    """
+
+    Parameters
+    ----------
+    n_samples: number of desired samples
+
+    Returns
+    -------
+    mat_U: 2xn_samples matrix of realizations of random vector U
+
+    """
+    U = np.array([0., 0.])
+
+    return U
