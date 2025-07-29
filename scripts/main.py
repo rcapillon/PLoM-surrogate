fe5d45cb0f193e51844459cc22d03bb915976a08
Rémi Capillon <rcapillon@yahoo.fr>
2025-07-29 22:52:26 +0200

    minor change

--- scripts/main.py
+++ scripts/main.py
@@ -7,10 +7,10 @@ from PLoM_surrogate import dmaps
 
 if __name__ == '__main__':
     x = np.linspace(0., 2., 100)
-    y = np.power(x, 2.)
+    y = x ** 2
 
     fig, ax = plt.subplots()
-    ax.plot(x, y, '-k', label='f(x)')
+    ax.plot(x, y, '-b', label='f(x)')
     ax.set_title('Graph of f(x) = x^2')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
