8ce88eb9a0b8e641c7d278f849d2b15f3d5a0339
Rémi Capillon <rcapillon@yahoo.fr>
2025-08-01 20:59:14 +0200

    work in progress

--- src/PLoM_surrogate/data.py
+++ src/PLoM_surrogate/data.py
@@ -119,12 +119,6 @@ class Dataset:
         self.vec_eig_X = eigvals[inds_sort_eig]
         self.mat_phi_X = eigvects[:, inds_sort_eig]
 
-        # inds_nonzero = np.argwhere(self.vec_eig_X > 0.).flatten()
-        # n_nonzero_eig_X = inds_nonzero.size
-        # mat_inv_sqrt_eig_X = np.zeros((self.vec_eig_X.size, self.vec_eig_X.size))
-        # mat_inv_sqrt_eig_X[:n_nonzero_eig_X, :n_nonzero_eig_X] = np.diag(1. / np.sqrt(self.vec_eig_X[inds_nonzero]))
-
-        # self.H_data = np.dot(mat_inv_sqrt_eig_X, np.dot(self.mat_phi_X.T, centered_X_data))
         self.H_data = np.dot(np.diag(1 / np.sqrt(self.vec_eig_X)), np.dot(self.mat_phi_X.T, centered_X_data))
 
     def recover_X(self, H):
--- src/PLoM_surrogate/dmaps.py
+++ src/PLoM_surrogate/dmaps.py
@@ -1,5 +1,75 @@
 import numpy as np
 
 
-def test(x):
-    return x
+def gaussian_kernel(vec1, vec2, eps):
+    k = (-1. / (4 * eps)) * np.linalg.norm(vec1 - vec2) ** 2
+
+    return k
+
+
+def build_mat_K(mat_eta, eps):
+    N = mat_eta.shape[1]
+    mat_K = np.zeros((N, N))
+    for i in range(N):
+        for j in range(N):
+            mat_K[i, j] = gaussian_kernel(mat_eta[:, i], mat_eta[:, j], eps)
+
+    return mat_K
+
+
+def build_mat_b(mat_K):
+    N = mat_K.shape[0]
+    mat_b = np.zeros((N, N))
+    for i in range(N):
+        mat_b[i, i] = np.sum(mat_K[i, :])
+
+    return mat_b
+
+
+def build_mat_Ps(mat_b, mat_K):
+    inv_sqrt_b = np.linalg.inv(np.sqrt(mat_b))
+    mat_Ps = np.dot(inv_sqrt_b, np.dot(mat_K, inv_sqrt_b))
+
+    return mat_Ps
+
+
+def construct_dmaps_basis(mat_eta, eps, m, kappa):
+    mat_K = build_mat_K(mat_eta, eps)
+    mat_b = build_mat_b(mat_K)
+    mat_Ps = build_mat_Ps(mat_b, mat_K)
+
+    eigvals, eigvects = np.linalg.eig(mat_Ps)
+    inds_sort_eigvals = np.flip(np.argsort(eigvals))
+    sorted_eigvals = S[inds_sort_eigvals]
+    sorted_eigvects = eigvects[:, inds_sort_eigvals]
+
+    vec_lambda = sorted_eigvals[:m]
+    mat_phi = sorted_eigvects[:, :m]
+
+    inv_sqrt_b = np.linalg.inv(np.sqrt(mat_b))
+    mat_psi = np.dot(inv_sqrt_b, mat_phi)
+
+    mat_g = np.zeros((mat_psi.shape[0], m))
+    for i in range(m):
+        mat_g[:, i] = (vec_lambda[i] ** kappa) * mat_psi[:, i]
+
+    return mat_g
+
+
+def build_mat_a(mat_g):
+    inv_gTg = np.linalg.inv(np.dot(mat_g.T, mat_g))
+    mat_a = np.dot(mat_g, inv_gTg)
+
+    return mat_a
+
+
+def project_on_dmaps(mat, mat_a):
+    projected_mat = np.dot(mat, mat_a)
+
+    return projected_mat
+
+
+def reverse_project_from_dmaps(projected_mat, mat_g):
+    mat = np.dot(projected_mat, mat_g.T)
+
+    return mat
