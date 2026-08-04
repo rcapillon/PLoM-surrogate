d4ce666a65978470b14ec847320620ea6aee5cdc
rcapillon <rcapillon@yahoo.fr>
2026-08-04 22:43:06 +0200

    wip dummy support class

--- src/PLoM_surrogate/supports.py
+++ src/PLoM_surrogate/supports.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 
+def support_R(x):
+    return x
+
+
+def support_Rplus(x):
+    pass
+
+
 class Support:
-    def __init__(self, supports=None):
+    def __init__(self, supports: list = None):
         """
 
 
